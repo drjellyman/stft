6da6a84a1cb2cd7943527c8b0aa24dde304a7b25
drjellyman <daylejellyman@hotmail.com>
2016-05-25 11:38:43 +1200

    vaguely started gsc

--- SpringerBF2.m
+++ SpringerBF2.m
@@ -48,7 +48,7 @@ zPos = [3,1,1 ; 4,1,1 ]';
 sPos = [3.5,3.5,1 ; 8,2,1]';
 NSensors = length(zPos(1,:));
 NSources = length(sPos(1,:));
-figure; plot(zPos(1,:),zPos(2,:),'o'); hold on; plot(sPos(1,:),sPos(2,:),'o'); xlim([0 10]); ylim([0 5]); legend('Sensors','Sources');
+% figure; plot(zPos(1,:),zPos(2,:),'o'); hold on; plot(sPos(1,:),sPos(2,:),'o'); xlim([0 10]); ylim([0 5]); legend('Sensors','Sources');
 
 % Construct observation signals using interpolation
 Fs = Fs1; % Fs sets the sample rate of the observations
@@ -60,35 +60,43 @@ nsWt = 0; % nsWt = noise weight
 c = 343; % c = velocity of sound in air (m.s^-1)
 for k = 1:K%K-1
     for m = 1:NSensors
-%         A(k,m) = exp(-j*2*pi*((k-1)*Fs/(K-1))*d(1,m)/c)/d(1,m);
-        A(k,m) = exp(-j*2*pi*((k-1)*Fs/(K-1))*0.5)/d(1,m);
+        A(k,m) = exp(-j*2*pi*((k-1)*Fs/(K-1))*d(1,m)/c)/d(1,m);
+%         A(k,m) = exp(-j*2*pi*((k-1)*Fs/(K-1))*0.5)/d(1,m);
     end
 end
-figure; plot(real(fftshift(A(:,1)))); legend('A(:,1)');
+% figure; plot(real(fftshift(A(:,1)))); legend('A(:,1)');
 
-ksym = [0:128,-128:-1];
-for k = 1:K%K-1%[0:127,-127:-1]%1:K-1
-    for m = 1:NSensors
-%         Asym(k,m) = exp(-j*2*pi*((ksym(k))*Fs/(K-1))*d(1,m)/c)/d(1,m);
-        Asym(k,m) = exp(-j*2*pi*((ksym(k))*Fs/(K))*0.5)/d(1,m);
-    end
-end
-figure; plot(real(fftshift(Asym(:,1))));legend('Asym(:,1)');
+%Attempt at making a conjugate symmetric ATF. There was a clash here
+%between the length of the window and the length of symmetric A. 
+% ksym = [0:128,-128:-1];
+% for k = 1:K%K-1%[0:127,-127:-1]%1:K-1
+%     for m = 1:NSensors
+% %         Asym(k,m) = exp(-j*2*pi*((ksym(k))*Fs/(K-1))*d(1,m)/c)/d(1,m);
+%         Asym(k,m) = exp(-j*2*pi*((ksym(k))*Fs/(K))*0.5)/d(1,m);
+%     end
+% end
+% figure; plot(real(fftshift(Asym(:,1))));legend('Asym(:,1)');
 
 % Use A to create the observations
-sPadded = [zeros((K-1)/2,NSensors) ; s ; zeros((K-1)/2,NSensors)];
-[S,LS] = stft(sPadded,K);
-for l = 1:LS
-    for m = 1:NSensors
-        ZA(:,l,m) = A(1:end-1,m).*S(:,l);
-    end
-end
-za = myNormalize(myOverlapAdd(ZA(:,:,1)));
-za = za(65:end);
+% sPadded = [zeros((K-1)/2,NSensors) ; s ; zeros((K-1)/2,NSensors)];
+% [S,LS] = stft(sPadded,K);
+% for l = 1:LS
+%     for m = 1:NSensors
+%         ZA(:,l,m) = A(1:end-1,m).*S(:,l);
+%     end
+% end
 
-zn = myNormalize(z(:,1)); s1n = myNormalize(s1); 
-figure; %plot(zn(358:end),'.'); 
-hold on; plot(s1n); plot(real(za)); legend('s1n','za');
+% This lot was comparing the signals shifted using A with the original
+% source (with only one source). The trick is to try and get A to be
+% conjugate symmetric so that the inverse FT results in an all real signal.
+% real(za)'*real(za)~e3 while imag(za)'*imag(za)~e-20 showing that almost
+% all of the energy was in the real part. 
+% za = myNormalize(myOverlapAdd(ZA(:,:,1)));
+% za = 1.12*za(65:end);
+
+% zn = myNormalize(z(:,1)); s1n = myNormalize(s1); 
+% figure; %plot(zn(358:end),'.'); 
+% hold on; plot(s1n); %plot(real(za)); legend('s1n','za');
 %plot(z(:,3));
 % plot(z(:,4));plot(z(:,5));
 % plot(z(:,6));plot(z(:,7));
@@ -103,15 +111,18 @@ zPadded = [zeros((K-1)/2,NSensors) ; z ; zeros((K-1)/2,NSensors)];
 %% Delay and Sum
 % What happens if I just find the weights that undo the delay? i.e. W^H*A =
 % I
-WH = pinv(A);
-Y = zeros(K-1,L);
-for l = 1:L
-    for k = 1:K-1
-        Y(k,l) = WH(:,k)' * squeeze(Z(k,l,:));
-    end
-end
-y = myNormalize(myOverlapAdd(Y));
-% plot(real(y(358:end)),'--')
+% WH = pinv(A);
+% Y = zeros(K-1,L);
+% for l = 1:L
+%     for k = 1:K-1
+%         Y(k,l) = WH(:,k)' * squeeze(Z(k,l,:));
+%     end
+% end
+% y = myNormalize(myOverlapAdd(Y)); 
+% plot(1.2*real(y(422:end)),'--') % The output looks close to to the source
+% signal, with only one input source. real(y)'*real(y)=2.44e3 while
+% imag(y)'*imag(y)=2.5e-20 showing that y is mostly real. 
+
 % y = y(590:end);
 % figure; plot(real(y)); hold on; %plot(s1); legend('y','s1');
 % cov(100*real(y((K-1)/2:end-(K-1)/2-1)),s1);
@@ -162,5 +173,24 @@ y = myNormalize(myOverlapAdd(Y));
 
 %% Springer GSC
 % Fixed bf
+% W0(k,l)=F(k)=A(k)/||A(k)||^2 Springer 47.20
+for k = 1:K
+    W0(k,:)=A(k,:)/(norm(A(k,:))^2);
+end
+% Yfbf(k,l)=W0^H(k)*Z(k,l);
+for l = 1:L
+    for k = 1:K-1
+        Yfbf(k,l,:) = conj(squeeze(W0(k,:)))*squeeze(Z(k,l,:));
+    end
+end
+yfbfHat = myOverlapAdd(Yfbf);
+yfbfHat = myNormalize(yfbfHat); s1n = myNormalize(s1); 
+figure; plot(1.5*real(yfbfHat(380:end))); hold on ; plot(s1n,'--'); legend('yfbfHat','s1n');
+% roughly one quarter of the energy is in the imaginary component, so there
+% is little more than wishful thinking suggesting that the signal will be
+% close to the original source.
+% real(yfbfHat)'*real(yfbfHat) 
+% imag(yfbfHat)'*imag(yfbfHat)
+% mySpectrogram(Yfbf);
 
 
